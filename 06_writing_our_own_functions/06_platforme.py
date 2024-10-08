db975900ab170f3b595d2f079525f9721a9b2b99
Yakimory <yastrebdev@gmail.com>
2024-10-08 18:13:35 +0700

    end two problem

--- 06_writing_our_own_functions/06_platforme.py
+++ 06_writing_our_own_functions/06_platforme.py
@@ -3,28 +3,45 @@
 
 def total_pillar_length(n, platforms):
     platforms.sort(reverse=True, key=lambda p: p[0])
-
     total_length = 0
 
     for i in range(n):
         y, x1, x2 = platforms[i]
-        left_pillar = y
-        right_pillar = y
+        if i == n -1:
+            total_length += y * 2
+            break
 
-        for j in range(i + 1, n):
-            y_below, x1_below, x2_below = platforms[j]
+        for x in ['x1', 'x2']:
+            for j in range(i + 1, n):
+                ly, lx1, lx2 = platforms[j]
+                if x == 'x1':
+                    if x1 < lx1:
+                        if j == n - 1:
+                            total_length += y
+                        else:
+                            continue
+                    else:
+                        total_length += y - ly
+                        break
 
-            if x1_below < x1 < x2_below:
-                left_pillar = min(left_pillar, y - y_below)
-
-            if x1_below < x2 < x2_below:
-                right_pillar = min(right_pillar, y - y_below)
-
-        total_length += left_pillar + right_pillar
+                if x == 'x2':
+                    if x2 > lx2:
+                        if j == n - 1:
+                            total_length += y
+                        else:
+                            continue
+                    elif x2 <= lx2:
+                        if x2 <= lx1:
+                            total_length += y
+                        else:
+                            total_length += y - ly
+                    else:
+                        total_length += y - ly
+                        break
 
     return total_length
 
 n = int(input())
-platforms = [tuple(map(int, input().split())) for _ in range(n)]
+platforms = [list(map(int, input().split())) for _ in range(n)]
 
 print(total_pillar_length(n, platforms))
--- 06_writing_our_own_functions/07_misa.py
+++ 06_writing_our_own_functions/07_misa.py
@@ -1,2 +1,41 @@
 # DMOJ problem - coci13c2p2
 # https://dmoj.ca/problem/coci13c2p2
+
+def count_touches_from_position(matrix, row, col):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    touches = 0
+
+    directions = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),         (0, 1),
+        (1, -1), (1, 0), (1, 1)
+    ]
+
+    for d in directions:
+        ni, nj = row + d[0], col + d[1]
+        if 0 <= ni < rows and 0 <= nj < cols and matrix[ni][nj] == 'o':
+            touches += 1
+
+    return touches
+
+
+def find_best_position(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    max_touches = -1
+
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == '.':
+                touches = count_touches_from_position(matrix, i, j)
+                if touches > max_touches:
+                    max_touches = touches
+
+    return max_touches
+
+
+r, s = map(int, input().split())
+rows = [list(input()) for _ in range(r)]
+
+print(find_best_position(rows))
