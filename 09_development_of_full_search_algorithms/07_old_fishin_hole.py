2f59f982c03449ac4566bb76b4d52f4ada0fb850
Yakimory <yastrebdev@gmail.com>
2024-10-30 18:25:51 +0700

    not solved

---
+++ 09_development_of_full_search_algorithms/07_old_fishin_hole.py
@@ -0,0 +1,13 @@
+# DMOJ problem - ccc09j2
+# https://dmoj.ca/problem/ccc09j2
+
+fish_points = [int(input()) for _ in range(4)]
+
+total_points = fish_points.pop()
+
+methods = 0
+
+combos = set()
+
+print(f'Number of ways to catch fish: {methods}')
+print(combos)
