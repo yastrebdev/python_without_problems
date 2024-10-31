fb634ac058e1ded8a3dab324358a078b5bdf0490
Yakimory <yastrebdev@gmail.com>
2024-10-31 16:23:12 +0700

    I finished reading the book

--- 09_development_of_full_search_algorithms/07_old_fishin_hole.py
+++ 09_development_of_full_search_algorithms/07_old_fishin_hole.py
@@ -1,13 +1,19 @@
 # DMOJ problem - ccc09j2
 # https://dmoj.ca/problem/ccc09j2
 
-fish_points = [int(input()) for _ in range(4)]
+trout_points = int(input())
+pike_points = int(input())
+pickerel_points = int(input())
+total_limit = int(input())
 
-total_points = fish_points.pop()
+count = 0
 
-methods = 0
+for trout in range(total_limit // trout_points + 1):
+    for pike in range(total_limit // pike_points + 1):
+        for pickerel in range(total_limit // pickerel_points + 1):
+            total_points = trout * trout_points + pike * pike_points + pickerel * pickerel_points
+            if 0 < total_points <= total_limit:
+                print(f"{trout} Brown Trout, {pike} Northern Pike, {pickerel} Yellow Pickerel")
+                count += 1
 
-combos = set()
-
-print(f'Number of ways to catch fish: {methods}')
-print(combos)
+print("Number of ways to catch fish:", count)
